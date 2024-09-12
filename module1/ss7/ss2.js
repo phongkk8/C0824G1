40108d1447d0665c52a349f8c562de5902e72b74
phongkk8 <phongly1194@gmail.com>
2024-09-12 13:58:05 +0700

    Up bai tap

---
+++ module1/ss7/ss2.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("Nhập chiều rộng");
+inputHeight = prompt("nhập chiều dài");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích HCN là: " + area);
